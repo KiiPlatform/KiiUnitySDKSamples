60dbd78882e3a791e3be4bd0298b032bc4b72dfd
Ryuji OCHI <ryuji.ochi@kii.com>
2013-12-19 18:29:07 +0900

    Initial commit of KiiUnitySDKSamples.

---
+++ AsyncDemo/Assets/MainCamera.cs
@@ -0,0 +1,55 @@
+﻿using KiiCorp.Cloud.Storage;
+
+using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	private const string APP_ID = "e8e2563e";
+	private const string APP_KEY = "a084a2fe9957f78cc6aeae264000eb12";
+	private const Kii.Site APP_SITE = Kii.Site.US;
+
+	private IPage currentPage;
+	private Stack pageStack = new Stack();
+
+	public GUISkin DemoGUISkin;
+
+	void Awake ()
+	{
+		Debug.Log("Initialize");
+
+		Kii.Initialize(APP_ID, APP_KEY, APP_SITE);
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentPage = new TitlePage(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		GUI.skin = DemoGUISkin;
+
+		currentPage.OnGUI();
+	}
+
+	public void PushPage(IPage next)
+	{
+		pageStack.Push(currentPage);
+		currentPage = next;
+	}
+
+	public void PopPage()
+	{
+		if (pageStack.Count == 0)
+		{
+			Debug.Log("Stack is empty!");
+			return;
+		}
+		currentPage = (IPage)pageStack.Pop();
+	}
+}
