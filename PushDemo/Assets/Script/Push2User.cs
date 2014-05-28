63ce075ea7011050fc125c1e656e21363be74cd1
Ryuji OCHI <ryuji.ochi@kii.com>
2014-05-28 18:27:15 +0900

    Remove old initialization code from PushDemo Push2App/Push2User scripts.

--- PushDemo/Assets/Script/Push2User.cs
+++ PushDemo/Assets/Script/Push2User.cs
@@ -22,8 +22,6 @@ public class Push2User : MonoBehaviour
 
     void Awake ()
     {
-        // Debug.Log ("#####Main.Awake");
-        // Kii.Initialize ("f39c2d34", "2e98ef0bb78a58da92f9ac0709dc99ed", Kii.Site.JP);
     }
 
     // Use this for initialization
