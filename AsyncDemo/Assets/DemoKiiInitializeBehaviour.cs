094c6ebde5c087863ae55e227d0a0532471dd742
Noriyoshi Fukuzaki <noriyoshi.fukuzaki@kii.com>
2014-06-11 16:47:40 +0900

    Set AppID, AppKey and Site to KiiInitializeBehaviour forcibly

---
+++ AsyncDemo/Assets/DemoKiiInitializeBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using KiiCorp.Cloud.Unity;
+
+public class DemoKiiInitializeBehaviour : KiiInitializeBehaviour {
+	public override void Awake()
+	{
+		this.AppID = "e8e2563e";
+		this.AppKey = "a084a2fe9957f78cc6aeae264000eb12";
+		this.Site = KiiCorp.Cloud.Storage.Kii.Site.US;
+		base.Awake();
+	}
+}
